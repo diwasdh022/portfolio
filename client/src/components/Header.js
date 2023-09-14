8e5cecf500b9567f13bbc21fa9780366096c8d8d
Diwas Dhakal <diwasdhakal75@gmail.com>
2023-09-14 14:48:59 +0545

    Init

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,15 +1,4 @@
-//fetch data from api
-async function getData() {
-    const res = await fetch('/api')
-    if (!res.ok) {
-        // This will activate the closest `error.js` Error Boundary
-        throw new Error('Failed to fetch data')
-    }
-    return res.json()
-}
 export default function Header() {
-    const data = getData()
-    console.log(data)
     return (
         //Header for a portfolio
         <div className="fullPageHeader">
